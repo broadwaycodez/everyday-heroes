b309ffc5ce6f7b3f4742660eb5f963bc3932efb3
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-12 13:29:11 -0400

    delivered streak info to ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,13 +2,19 @@ import React from 'react'
 import './ProgressBar.css'
 import Utils from '../../utils/utils'
 
-const ProgressBar = ({title, earned, required}) => {
+const ProgressBar = ({title, earned, required, streak}) => {
   const percent = Utils.getPercent(earned, required)
+  console.log(streak)
   return (
     <div className="ProgressBar">
       <h4 className="progressBar__heading">{Utils.capitalize(title)}</h4>
       <div className="progressBar__holder">
-        <div className="progressBar__meter-title">{earned}/{required}</div>
+        <div className="progressBar__meter-title">
+          <div>{earned}/{required}</div>
+          {streak && (
+            <div className="progressBar__streak">Streak: {streak}</div>
+          )}
+        </div>
         <div className="progressBar__measurement" style={{width: `${percent}%`}}></div>
       </div>
     </div>
